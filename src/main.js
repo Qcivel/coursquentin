0f58d5b3230d5a1b63f29117a3290b1de2ae96ef
Qcivel <q.civel@yahoo.fr>
2025-09-25 10:13:55 +0200

    ex_09

--- src/main.js
+++ src/main.js
@@ -1,13 +1,37 @@
 import './style.css'
 import { apiKey } from './env.js'
 
-const meteo = fetch('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=270fc396718e566e1382770e7dc2bcbc')
-  .then(response => {
-    console.log(response)
-    return response
-  })
-  .then(data=>{
-    return data.json();
-  });
+// const meteo = fetch('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid='+apiKey)
+//   .then(response => {
+//     console.log(response)
+//     return response
+//   })
+//   .then(data=>{
+//     return data.json();
+//   });
 
-  console.log(meteo);
+//   console.log(meteo);
+// avec une fonction fléché
+//   const apiMeteo = async () =>{//deuxiemme façon de traiter une requete avec sa reponse
+//   return await fetch('https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid='+apiKey)
+//     .then(response => {
+//         return response.json();//avec console log on peut choisir 
+//     });
+// }
+// apiMeteo().then(json => {
+//   console.log(json);
+// })
+
+// Avec une fonction normal
+async function apiFunction() {
+    return await fetch('https://api.openweathermap.org/data/2.5/weather?lang=fr&q=paris&appid='+apiKey)
+    .then(response => {
+        return response.json();
+    });
+}
+
+apiFunction().then(json => {
+  console.log(json.name);
+  console.log(json.coord.lon);
+  console.log(json.weather[0].main);
+}) 
